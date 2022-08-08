b85f2d01715d5ac53736478e0eb767661953ba1d
strongbaass <ggg266182@gmail.com>
2022-08-08 00:29:37 -0400

    auth

---
+++ src/redux/slices/auth.js
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchAuth = createAsyncThunk(
+  "auth/FetchUserData",
+  async (params) => {
+    const { data } = await axios.post(
+      "http://localhost:4444/auth/login",
+      params
+    );
+    return data;
+  }
+);
+
+const initialState = {
+  data: null,
+  status: "loading",
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  extraReducers: {
+    [fetchAuth.pending]: (state) => {
+      state.status = "loading";
+      state.data = null;
+    },
+    [fetchAuth.fulfilled]: (state, action) => {
+      state.status = "loaded";
+      state.data = action.payload;
+    },
+    [fetchAuth.rejected]: (state) => {
+      state.status = "error";
+      state.data = null;
+    },
+  },
+});
+
+export const authReducer = authSlice.reducer;
